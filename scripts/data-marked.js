aa25fc3bff80ba387e33c71307ebfcd21fa21316
Andrew McWatters <mcwattersandrew@gmail.com>
2018-03-23 10:08:51 -0700

    Remove repositories in favor of GitHub repositories

--- scripts/data-marked.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-document.body.addEventListener('includecontentloaded', function(event) {
-  var el = event.target;
-  if (el.dataset.marked === '') {
-    el.innerHTML = marked(el.innerHTML);
-  };
-});
