d7e9acb17691c254d05f13469525bbf8ea4c8b52
Andrew McWatters <mcwattersandrew@gmail.com>
2022-12-17 15:37:22 -0700

    Add filters to compare

--- javascripts/compare.js
+++ javascripts/compare.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -60,97 +60,217 @@ var Compare = function (_React$Component) {
         React.Fragment,
         null,
         React.createElement(
-          "h1",
-          null,
-          "Compare game software"
-        ),
-        error ? React.createElement(
           "div",
-          null,
-          "Error: ",
-          error.message
-        ) : !isLoaded ? React.createElement(
-          "div",
-          null,
-          "Loading..."
-        ) :
-        // <ul>
-        //   {items.map(item => (
-        //     <li key={item.id}>
-        //       {item.name}<br /><span className="text-muted">({item.type})</span>
-        //     </li>
-        //   ))}
-        // </ul>
-        React.createElement(
-          "table",
-          { className: "table" },
+          { className: "row" },
           React.createElement(
-            "thead",
-            null,
+            "h1",
+            { className: "mb-3" },
+            "Compare game software"
+          )
+        ),
+        React.createElement(
+          "div",
+          { className: "row" },
+          React.createElement(
+            "div",
+            { className: "col-md-3 col-xl-2" },
             React.createElement(
-              "tr",
-              null,
+              "div",
+              { className: "sticky-top pt-5" },
               React.createElement(
-                "th",
-                { scope: "col" },
-                "#"
+                "p",
+                null,
+                React.createElement(
+                  "b",
+                  null,
+                  "Filters"
+                )
               ),
               React.createElement(
-                "th",
-                { scope: "col" },
-                "Name"
-              ),
-              React.createElement(
-                "th",
-                { scope: "col" },
+                "p",
+                { className: "mb-2" },
                 "Type"
               ),
               React.createElement(
-                "th",
-                { scope: "col" },
+                "p",
+                null,
+                React.createElement(
+                  "select",
+                  {
+                    className: "form-select",
+                    name: "type"
+                    // value={filters.type}
+                    // onChange={this.handleFilterChange}
+                    , disabled: true
+                  },
+                  React.createElement(
+                    "option",
+                    null,
+                    "None"
+                  ),
+                  React.createElement(
+                    "option",
+                    null,
+                    "Game engine"
+                  ),
+                  React.createElement(
+                    "option",
+                    null,
+                    "Game framework"
+                  )
+                )
+              ),
+              React.createElement(
+                "p",
+                { className: "mb-2" },
                 "Primary Language"
               ),
               React.createElement(
-                "th",
-                { scope: "col" },
+                "p",
+                null,
+                React.createElement(
+                  "select",
+                  {
+                    className: "form-select",
+                    name: "primary_language"
+                    // value={filters.primary_language}
+                    // onChange={this.handleFilterChange}
+                    , disabled: true
+                  },
+                  React.createElement(
+                    "option",
+                    null,
+                    "None"
+                  )
+                )
+              ),
+              React.createElement(
+                "p",
+                { className: "mb-2" },
                 "Perspective"
+              ),
+              React.createElement(
+                "p",
+                null,
+                React.createElement(
+                  "div",
+                  { className: "form-check" },
+                  React.createElement("input", {
+                    className: "form-check-input",
+                    type: "checkbox",
+                    value: "",
+                    id: "perspective2d",
+                    disabled: true
+                  }),
+                  React.createElement(
+                    "label",
+                    { className: "form-check-label", "for": "perspective2d" },
+                    "2D"
+                  )
+                ),
+                React.createElement(
+                  "div",
+                  { className: "form-check" },
+                  React.createElement("input", {
+                    className: "form-check-input",
+                    type: "checkbox",
+                    value: "",
+                    id: "perspective3d",
+                    disabled: true
+                  }),
+                  React.createElement(
+                    "label",
+                    { className: "form-check-label", "for": "perspective3d" },
+                    "3D"
+                  )
+                )
               )
             )
           ),
           React.createElement(
-            "tbody",
-            null,
-            items.map(function (item) {
-              return React.createElement(
-                "tr",
-                { key: item.id },
+            "div",
+            { className: "col-md-9 col-xl-10" },
+            error ? React.createElement(
+              "div",
+              null,
+              "Error: ",
+              error.message
+            ) : !isLoaded ? React.createElement(
+              "div",
+              null,
+              "Loading..."
+            ) : React.createElement(
+              "table",
+              { className: "table" },
+              React.createElement(
+                "thead",
+                null,
                 React.createElement(
-                  "th",
-                  { scope: "row" },
-                  item.id
-                ),
-                React.createElement(
-                  "td",
+                  "tr",
                   null,
-                  item.name
-                ),
-                React.createElement(
-                  "td",
-                  null,
-                  item.type
-                ),
-                React.createElement(
-                  "td",
-                  null,
-                  item.primary_language
-                ),
-                React.createElement(
-                  "td",
-                  null,
-                  item.perspective
+                  React.createElement(
+                    "th",
+                    { scope: "col" },
+                    "#"
+                  ),
+                  React.createElement(
+                    "th",
+                    { scope: "col" },
+                    "Name"
+                  ),
+                  React.createElement(
+                    "th",
+                    { scope: "col" },
+                    "Type"
+                  ),
+                  React.createElement(
+                    "th",
+                    { scope: "col" },
+                    "Primary Language"
+                  ),
+                  React.createElement(
+                    "th",
+                    { scope: "col" },
+                    "Perspective"
+                  )
                 )
-              );
-            })
+              ),
+              React.createElement(
+                "tbody",
+                null,
+                items.map(function (item) {
+                  return React.createElement(
+                    "tr",
+                    { key: item.id },
+                    React.createElement(
+                      "th",
+                      { scope: "row" },
+                      item.id
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      item.name
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      item.type
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      item.primary_language
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      item.perspective
+                    )
+                  );
+                })
+              )
+            )
           )
         )
       );
@@ -160,5 +280,5 @@ var Compare = function (_React$Component) {
   return Compare;
 }(React.Component);
 
-var domContainer = document.querySelector('#compare_container');
+var domContainer = document.querySelector("#compare_container");
 ReactDOM.render(React.createElement(Compare, null), domContainer);
--- javascripts/src/compare.js
+++ javascripts/src/compare.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 class Compare extends React.Component {
   constructor(props) {
@@ -6,19 +6,19 @@ class Compare extends React.Component {
     this.state = {
       error: null,
       isLoaded: false,
-      items: []
+      items: [],
     };
   }
 
   componentDidMount() {
     fetch("/data/game-software.json")
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
         (result) => {
           this.setState({
             isLoaded: true,
             // items: result.items
-            items: result
+            items: result,
           });
         },
         // Note: it's important to handle errors here
@@ -27,73 +27,115 @@ class Compare extends React.Component {
         (error) => {
           this.setState({
             isLoaded: true,
-            error
+            error,
           });
         }
-      )
+      );
   }
 
   render() {
     const { error, isLoaded, items } = this.state;
     return (
       <React.Fragment>
-        <h1>Compare game software</h1>
-        {error ? (
-          <div>Error: {error.message}</div>
-        ) : !isLoaded ? (
-          <div>Loading...</div>
-        ) : (
-          // <ul>
-          //   {items.map(item => (
-          //     <li key={item.id}>
-          //       {item.name}<br /><span className="text-muted">({item.type})</span>
-          //     </li>
-          //   ))}
-          // </ul>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Type</th>
-                <th scope="col">Primary Language</th>
-                <th scope="col">Perspective</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-              </tr> */}
-             {items.map(item => (
-               <tr key={item.id}>
-                <th scope="row">{item.id}</th>
-                <td>{item.name}</td>
-                <td>{item.type}</td>
-                <td>{item.primary_language}</td>
-                <td>{item.perspective}</td>
-               </tr>
-             ))}
-            </tbody>
-          </table>
-      )}
+        <div className="row">
+          <h1 className="mb-3">Compare game software</h1>
+        </div>
+        <div className="row">
+          <div className="col-md-3 col-xl-2">
+            <div className="sticky-top pt-5">
+              <p>
+                <b>Filters</b>
+              </p>
+              <p className="mb-2">Type</p>
+              <p>
+                <select
+                  className="form-select"
+                  name="type"
+                  // value={filters.type}
+                  // onChange={this.handleFilterChange}
+                  disabled
+                >
+                  <option>None</option>
+                  <option>Game engine</option>
+                  <option>Game framework</option>
+                </select>
+              </p>
+              <p className="mb-2">Primary Language</p>
+              <p>
+                <select
+                  className="form-select"
+                  name="primary_language"
+                  // value={filters.primary_language}
+                  // onChange={this.handleFilterChange}
+                  disabled
+                >
+                  <option>None</option>
+                </select>
+              </p>
+              <p className="mb-2">Perspective</p>
+              <p>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="perspective2d"
+                    disabled
+                  />
+                  <label className="form-check-label" for="perspective2d">
+                    2D
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="perspective3d"
+                    disabled
+                  />
+                  <label className="form-check-label" for="perspective3d">
+                    3D
+                  </label>
+                </div>
+              </p>
+            </div>
+          </div>
+          <div className="col-md-9 col-xl-10">
+            {error ? (
+              <div>Error: {error.message}</div>
+            ) : !isLoaded ? (
+              <div>Loading...</div>
+            ) : (
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Primary Language</th>
+                    <th scope="col">Perspective</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {items.map((item) => (
+                    <tr key={item.id}>
+                      <th scope="row">{item.id}</th>
+                      <td>{item.name}</td>
+                      <td>{item.type}</td>
+                      <td>{item.primary_language}</td>
+                      <td>{item.perspective}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </div>
       </React.Fragment>
     );
   }
 }
 
-let domContainer = document.querySelector('#compare_container');
+let domContainer = document.querySelector("#compare_container");
 ReactDOM.render(<Compare />, domContainer);
